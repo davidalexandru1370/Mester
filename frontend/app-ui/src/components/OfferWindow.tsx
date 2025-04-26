43ca5cada0ee00a4239893d8db594cf7e478bc70
Popovici George <57073444+Nyesteban@users.noreply.github.com>
2025-04-26 17:26:17 +0300

    Foarte basic template

---
+++ frontend/app-ui/src/components/OfferWindow.tsx
@@ -0,0 +1,22 @@
+import './ClassifiedTemplate.css'
+
+export default function OfferWindow() {
+    return (
+    <div className="single_ads_card mt-30 border">
+        <div className="ads_card_image">
+            <img src="..\src\assets\images\ads-1.png" alt="ads" />
+        </div>
+        <div className="ads_card_content">
+            <div className="meta d-flex justify-content-between">
+                <p>Frec menta</p>
+            </div>
+        <h4 className="title"><a href="product-details.html">Stima si respect</a></h4>
+        <p><i className="far fa-map-marker-alt"></i>Cluj-Napoca, Cluj</p>
+        <div className="ads_price_date d-flex justify-content-between">
+            <span className="price">2 RON</span>
+            <span className="date">21 Apr, 2025</span>
+        </div>
+        </div>
+    </div>
+    )
+}
